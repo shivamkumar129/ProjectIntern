bba3c2cb5517fdc2c8b2ace09ad26efb76f68880
shivamkumar129 <150353644+shivamkumar129@users.noreply.github.com>
2024-09-09 18:30:55 +0530

    Add files via upload

---
+++ WeatherApp/weather.js
@@ -0,0 +1,28 @@
+ const apiKey = '8c7f8e76933e3a98c2fa1cbf5a9a1823';
+const apiUrl ="";
+const searchButton = document.getElementById('searchButton');
+const locationElement = document.getElementById('location');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
+
+searchButton.addEventListener('click', () => {
+    const location = locationInput.value;
+    if (location) {
+        fetchWeather(location);
+    }
+});
+
+ function fetchWeather(location) {
+    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            locationElement.textContent = data.name;
+            temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+            descriptionElement.textContent = data.weather[0].description;
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
+}
